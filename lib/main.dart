39714bf239bf99a61543c16fe2c26ebcd7c6c056
johnnylongvo <johnnylongvo@gmail.com>
2023-02-15 13:32:52 -0600

    creating AppBar & SearchBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:capstone/view/todo_screen.dart';
 
-import 'controller/constant.dart';
+import 'model/constant.dart';
 
 void main() {
   runApp(const PetPlanner());
@@ -15,46 +15,46 @@ class PetPlanner extends StatelessWidget {
     //return const MaterialApp(
     return MaterialApp(
       //debugShowCheckedModeBanner: false,
-      debugShowCheckedModeBanner: Constant.dev,
-      theme: ThemeData(
-        brightness: Brightness.dark,
-        fontFamily: 'LobsterTwo',
-        textTheme: const TextTheme(
-          headline1: TextStyle(
-            fontSize: 64.0,
-            color: Colors.red,
-          ),
-          button: TextStyle(
-            fontSize: 32.0,
-          ),
-        ),
-        elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ElevatedButton.styleFrom(
-            primary: Colors.teal,
-            elevation: 12.0,
-            shadowColor: Colors.yellowAccent,
-            textStyle: const TextStyle(
-              fontSize: 28.0,
-            ),
-          ),
-        ),
-        outlinedButtonTheme: OutlinedButtonThemeData(
-          style: OutlinedButton.styleFrom(
-            primary: Colors.white,
-            backgroundColor: Colors.blueAccent,
-            textStyle: const TextStyle(fontSize: 20.0),
-          ),
-        ),
-        textButtonTheme: TextButtonThemeData(
-          style: TextButton.styleFrom(
-            primary: Colors.yellowAccent,
-            backgroundColor: Colors.grey,
-            textStyle: const TextStyle(
-              fontSize: 18.0,
-            ),
-          ),
-        ),
-      ),
+      // debugShowCheckedModeBanner: Constant.dev,
+      // theme: ThemeData(
+      //   brightness: Brightness.dark,
+      //   fontFamily: 'LobsterTwo',
+      //   textTheme: const TextTheme(
+      //     headline1: TextStyle(
+      //       fontSize: 64.0,
+      //       color: Colors.red,
+      //     ),
+      //     button: TextStyle(
+      //       fontSize: 32.0,
+      //     ),
+      //   ),
+      //   elevatedButtonTheme: ElevatedButtonThemeData(
+      //     style: ElevatedButton.styleFrom(
+      //       primary: Colors.teal,
+      //       elevation: 12.0,
+      //       shadowColor: Colors.yellowAccent,
+      //       textStyle: const TextStyle(
+      //         fontSize: 28.0,
+      //       ),
+      //     ),
+      //   ),
+      //   outlinedButtonTheme: OutlinedButtonThemeData(
+      //     style: OutlinedButton.styleFrom(
+      //       primary: Colors.white,
+      //       backgroundColor: Colors.blueAccent,
+      //       textStyle: const TextStyle(fontSize: 20.0),
+      //     ),
+      //   ),
+      //   textButtonTheme: TextButtonThemeData(
+      //     style: TextButton.styleFrom(
+      //       primary: Colors.yellowAccent,
+      //       backgroundColor: Colors.grey,
+      //       textStyle: const TextStyle(
+      //         fontSize: 18.0,
+      //       ),
+      //     ),
+      //   ),
+      // ),
       initialRoute: ToDoScreen.routeName,
       //home: Text('Hello World'),
       routes: {
--- lib/view/todo_screen.dart
+++ lib/view/todo_screen.dart
@@ -7,7 +7,76 @@ class ToDoScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Scaffold(
+      backgroundColor: Colors.grey[400],
+      appBar: AppBar(
+        backgroundColor: Colors.purple[300],
+        title: Row(
+          children: const [
+            Icon(
+              Icons.menu,
+              color: Colors.black,
+            ),
+            Text(
+              'To Do List',
+              textAlign: TextAlign.center,
+            ),
+          ],
+        ),
+      ),
+      //body: const Text('List begins here'),
+      body: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 17),
+        child: Column(
+          children: [
+            searchBox(),
+            Expanded(
+              child: ListView(
+                children: [
+                  Container(
+                    margin: const EdgeInsets.only(
+                      top: 50,
+                      bottom: 20,
+                    ),
+                    child: const Text(
+                      'My ToDos',
+                      style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
+}
+
+Widget searchBox() {
+  return Container(
+    decoration: BoxDecoration(
+      color: Colors.white,
+      borderRadius: BorderRadius.circular(20),
+    ),
+    child: const TextField(
+      decoration: InputDecoration(
+        contentPadding: EdgeInsets.all(0),
+        prefixIcon: Icon(
+          Icons.search_rounded,
+          color: Colors.black,
+          size: 25,
+        ),
+        prefixIconConstraints: BoxConstraints(
+          maxHeight: 20,
+          minWidth: 25,
+        ),
+        border: InputBorder.none,
+        hintText: ('Search Here'),
+      ),
+    ),
+  );
 }
