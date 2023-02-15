4b365e7e4fc9bdd591563415bfd73f48cf70534b
johnnylongvo <johnnylongvo@gmail.com>
2023-02-15 12:09:34 -0600

    Initial Commit - ToDoScreen setup

---
+++ lib/view/todo_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ToDoScreen extends StatelessWidget {
+  static const routeName = '/todoScreen';
+
+  const ToDoScreen({Key? key}) : super(key: key); //constructor
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
