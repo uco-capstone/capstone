755e549b0eb3c46fcbacfc71882c148bb060ab80
EsromGile <elimorse15@gmail.com>
2023-02-16 09:40:55 -0600

    updated dependencies and resolved conflicts

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 
+import 'package:capstone/view/home_screen.dart';
 import 'package:capstone/view/login/login_screen.dart';
 import 'package:flutter/material.dart';
 
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,15 +6,14 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:capstone/main.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:capstone/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const PetPlanner());
+    await tester.pumpWidget(const CapstoneApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
