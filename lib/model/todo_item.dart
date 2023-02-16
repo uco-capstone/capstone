9d19acb6da34161748abac5629882a957cf61960
johnnylongvo <johnnylongvo@gmail.com>
2023-02-16 13:37:27 -0600

    add new task field & 'add' task button

--- lib/model/todo_item.dart
+++ lib/model/todo_item.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
 
 class ToDoItem extends StatelessWidget {
   const ToDoItem({Key? key}) : super(key: key);
@@ -8,38 +6,36 @@ class ToDoItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     //return const Text('TODO Item');
-    return Container(
-      child: ListTile(
-        onTap: () {},
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(20),
+    return ListTile(
+      onTap: () {},
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(20),
+      ),
+      tileColor: Colors.white,
+      leading: const Icon(
+        Icons.check_box,
+        color: Colors.blue,
+      ),
+      title: const Text(
+        'Feed Kirby',
+        style: TextStyle(
+          fontSize: 17,
+          color: Colors.black,
+          //decoration: TextDecoration.lineThrough,
         ),
-        tileColor: Colors.white,
-        leading: const Icon(
-          Icons.check_box,
-          color: Colors.blue,
+      ),
+      trailing: Container(
+        height: 35,
+        width: 35,
+        decoration: BoxDecoration(
+          color: Colors.red,
+          borderRadius: BorderRadius.circular(5),
         ),
-        title: const Text(
-          'Feed Kirby',
-          style: TextStyle(
-            fontSize: 17,
-            color: Colors.black,
-            //decoration: TextDecoration.lineThrough,
-          ),
-        ),
-        trailing: Container(
-          height: 35,
-          width: 35,
-          decoration: BoxDecoration(
-            color: Colors.red,
-            borderRadius: BorderRadius.circular(5),
-          ),
-          child: IconButton(
-            color: Colors.white,
-            icon: const Icon(Icons.delete),
-            iconSize: 17,
-            onPressed: () {},
-          ),
+        child: IconButton(
+          color: Colors.white,
+          icon: const Icon(Icons.delete),
+          iconSize: 17,
+          onPressed: () {},
         ),
       ),
     );
--- lib/view/todo_screen.dart
+++ lib/view/todo_screen.dart
@@ -22,57 +22,123 @@ class ToDoScreen extends StatelessWidget {
         ],
       ),
       //body: const Text('To Do Items'),
-      body: Container(
-        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
-        child: Column(
-          children: [
-            Container(
-              decoration: BoxDecoration(
-                color: Colors.grey[300],
-                borderRadius: BorderRadius.circular(20),
-              ),
-              child: const TextField(
-                decoration: InputDecoration(
-                  prefixIcon: Icon(
-                    Icons.search,
-                    size: 25,
-                    color: Colors.deepPurple,
-                  ),
-                  contentPadding: EdgeInsets.all(0),
-                  prefixIconConstraints: BoxConstraints(
-                    maxHeight: 10,
-                    minWidth: 25,
-                  ),
-                  border: InputBorder.none,
-                  hintText: 'Search Keywords',
-                  hintStyle: TextStyle(
-                    color: Colors.grey,
-                  ),
-                ),
-              ),
+      body: Stack(
+        children: [
+          Container(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 20,
+              vertical: 15,
             ),
-            Expanded(
-              child: ListView(
-                children: [
-                  Container(
-                    margin: const EdgeInsets.only(
-                      top: 50,
-                      bottom: 20,
-                    ),
-                    child: const Text(
-                      'All ToDos',
-                      style: TextStyle(
-                        fontSize: 30,
-                        fontWeight: FontWeight.w500,
+            child: Column(
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    color: Colors.grey[300],
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: const TextField(
+                    decoration: InputDecoration(
+                      prefixIcon: Icon(
+                        Icons.search,
+                        size: 27,
+                        color: Colors.deepPurple,
+                      ),
+                      contentPadding: EdgeInsets.all(0),
+                      prefixIconConstraints: BoxConstraints(
+                        maxHeight: 10,
+                        minWidth: 25,
+                      ),
+                      border: InputBorder.none,
+                      hintText: 'Search Keywords',
+                      hintStyle: TextStyle(
+                        color: Colors.grey,
                       ),
                     ),
                   ),
-                  const ToDoItem(),
-                ],
-              ),
+                ),
+                Expanded(
+                  child: ListView(
+                    children: [
+                      Container(
+                        margin: const EdgeInsets.only(
+                          top: 50,
+                          bottom: 20,
+                        ),
+                        child: const Text(
+                          'All ToDos',
+                          style: TextStyle(
+                            fontSize: 30,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                      ),
+                      const ToDoItem(),
+                    ],
+                  ),
+                ),
+              ],
             ),
-          ],
-        ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    margin: const EdgeInsets.only(
+                      bottom: 20,
+                      left: 20,
+                      right: 20,
+                    ),
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 20,
+                      vertical: 5,
+                    ),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      // ignore: prefer_const_literals_to_create_immutables
+                      boxShadow: [
+                        const BoxShadow(
+                          color: Colors.grey,
+                          offset: Offset(0.0, 0.0),
+                          blurRadius: 10.0,
+                          spreadRadius: 0.0,
+                        ),
+                      ],
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: const TextField(
+                      decoration: InputDecoration(
+                        hintText: 'Add New Task',
+                        border: InputBorder.none,
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: const EdgeInsets.only(
+                    bottom: 20,
+                    right: 20,
+                  ),
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.purple[200],
+                      minimumSize: const Size(60, 60),
+                      elevation: 10,
+                    ),
+                    child: const Text(
+                      '+',
+                      style: TextStyle(
+                        fontSize: 42,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
