94015f82ceaeb6aecbe4e51b2567601d246b83bc
EsromGile <elimorse15@gmail.com>
2023-02-16 09:29:42 -0600

    updated widget_test to have correct app name

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:capstone/view/home_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
