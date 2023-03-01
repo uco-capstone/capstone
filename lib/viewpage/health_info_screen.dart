113426f9d79a33fc9703768258bbdc6cb3b6aa8d
Eli Morse <elimorse15+github@gmail.com>
2023-03-01 13:53:03 -0600

    renamed health info screen

--- lib/viewpage/health_info_screen.dart
+++ lib/viewpage/health_info_screen.dart
@@ -1,5 +1,5 @@
 import 'package:capstone/controller/firestore_controller.dart';
-import 'package:capstone/model/health_screen_model.dart';
+import 'package:capstone/model/health_info_screen_model.dart';
 import 'package:capstone/model/kirby_user_model.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
