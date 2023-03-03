53aaa2b0b694b593254233e95d0d96ea41b9dd5f
johnnylongvo <johnnylongvo@gmail.com>
2023-03-02 22:53:22 -0600

    Screen Setup: BottomNavigationBar added

--- lib/viewpage/store_screen.dart
+++ lib/viewpage/store_screen.dart
@@ -21,15 +21,72 @@ class _StoreScreen extends State<StoreScreen> {
   }
 
   void render(fn) => setState(fn);
+  int _selectedIndex = 0;
+
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'Index 0: Colors',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 1: Background',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 2: Accesories',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 3: Food',
+      style: optionStyle,
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Sign In'),
+        title: const Text('Store Screen'),
       ),
-      //body: const Text('Start Screen works!'),
-      body: const Text('Store Screen'),
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+            backgroundColor: Colors.red,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.business),
+            label: 'Business',
+            backgroundColor: Colors.green,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.school),
+            label: 'School',
+            backgroundColor: Colors.purple,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Settings',
+            backgroundColor: Colors.pink,
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        onTap: _onItemTapped,
+      ),
     );
   }
 }
