44b9f385f1bb743cff90b87083460835976d6e9c
johnnylongvo <johnnylongvo@gmail.com>
2023-03-22 22:17:59 -0500

    screen UI setup finished.

--- lib/viewpage/achievement_screen.dart
+++ lib/viewpage/achievement_screen.dart
@@ -37,25 +37,155 @@ class _AchievementScreenState extends State<AchievementScreen> {
           ),
         ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-              height: 100.0,
-              width: 100.0,
-              decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                border: Border.all(color: Colors.red),
-                image: const DecorationImage(
-                  image: AssetImage('images/crown.png'),
-                ),
-              ),
-            ),
-          ],
-        ),
+      body:
+          // Padding(
+          //   padding: const EdgeInsets.all(8.0),
+          //   child: Row(
+          //     mainAxisAlignment: MainAxisAlignment.center,
+          //     children: [
+          //       Container(
+          //         height: 100.0,
+          //         width: 100.0,
+          //         decoration: BoxDecoration(
+          //           shape: BoxShape.circle,
+          //           border: Border.all(color: Colors.red),
+          //           image: const DecorationImage(
+          //             image: AssetImage('images/crown.png'),
+          //           ),
+          //         ),
+          //       ),
+          GridView.count(
+        primary: false,
+        padding: const EdgeInsets.all(20),
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
+        crossAxisCount: 3,
+        children: <Widget>[
+          Container(
+            padding: const EdgeInsets.all(8),
+            color: Colors.orange[50],
+            child: const Text("Eat 3x a day"),
+          ),
+          Container(
+            padding: const EdgeInsets.all(8),
+            color: Colors.orange[50],
+            child: const Text('Eat 9x a day'),
+          ),
+          Container(
+            padding: const EdgeInsets.all(8),
+            color: Colors.orange[50],
+            child: const Text("Eat 12x a day"),
+          ),
+          Container(
+            padding: const EdgeInsets.all(8),
+            color: Colors.orange[100],
+            child: const Text('Spent 10 coins'),
+          ),
+          Container(
+            padding: const EdgeInsets.all(8),
+            color: Colors.orange[100],
+            child: const Text('Spent 50 coins'),
+          ),
+          Container(
+            padding: const EdgeInsets.all(8),
+            color: Colors.orange[100],
+            child: const Text('Spent 100 coins'),
+          ),
+          Container(
+            padding: const EdgeInsets.all(8),
+            color: Colors.orange[200],
+            child: const Text('Went for a walk 3x time'),
+          ),
+          Container(
+            padding: const EdgeInsets.all(8),
+            color: Colors.orange[200],
+            child: const Text('Went for a walk 6x time'),
+          ),
+          Container(
+            padding: const EdgeInsets.all(8),
+            color: Colors.orange[200],
+            child: const Text('Went for a walk 9x time'),
+          ),
+          Container(
+            padding: const EdgeInsets.all(8),
+            color: Colors.orange[300],
+            child: const Text('Drink 6oz of Water'),
+          ),
+          Container(
+            padding: const EdgeInsets.all(8),
+            color: Colors.orange[300],
+            child: const Text('Drank 32oz of Water'),
+          ),
+          Container(
+            padding: const EdgeInsets.all(8),
+            color: Colors.orange[300],
+            child: const Text('Drink 62oz of Water'),
+          ),
+          Container(
+            padding: const EdgeInsets.all(8),
+            color: Colors.orange[400],
+            child: const Text('Accomplishments'),
+          ),
+          Container(
+            padding: const EdgeInsets.all(8),
+            color: Colors.orange[400],
+            child: const Text('Accomplishments'),
+          ),
+          Container(
+            padding: const EdgeInsets.all(8),
+            color: Colors.orange[400],
+            child: const Text('Drink 62oz of Water'),
+          ),
+          Container(
+            padding: const EdgeInsets.all(8),
+            color: Colors.orange[500],
+            child: const Text('Accomplishments'),
+          ),
+          Container(
+            padding: const EdgeInsets.all(8),
+            color: Colors.orange[500],
+            child: const Text('Accomplishments'),
+          ),
+          Container(
+            padding: const EdgeInsets.all(8),
+            color: Colors.orange[500],
+            child: const Text('Accomplishments'),
+          ),
+          Container(
+            padding: const EdgeInsets.all(8),
+            color: Colors.orange[600],
+            child: const Text('Accomplishments'),
+          ),
+          Container(
+            padding: const EdgeInsets.all(8),
+            color: Colors.orange[600],
+            child: const Text('Accomplishments'),
+          ),
+          Container(
+            padding: const EdgeInsets.all(8),
+            color: Colors.orange[600],
+            child: const Text('Accomplishments'),
+          ),
+          Container(
+            padding: const EdgeInsets.all(8),
+            color: Colors.orange[800],
+            child: const Text('Accomplishments'),
+          ),
+          Container(
+            padding: const EdgeInsets.all(8),
+            color: Colors.orange[800],
+            child: const Text('Accomplishments'),
+          ),
+          Container(
+            padding: const EdgeInsets.all(8),
+            color: Colors.orange[800],
+            child: const Text('Accomplishments'),
+          ),
+        ],
       ),
+      // ],
+      // ),
+      // ),
     );
   }
 }
