e7ee07354ed5ef56aad00001fa45d9396f1a5e10
Lisa Sun <sunlisa@gmail.com>
2023-03-23 01:46:33 -0500

    mock chart

--- lib/viewpage/history_screen.dart
+++ lib/viewpage/history_screen.dart
@@ -1,11 +1,11 @@
 import 'package:capstone/controller/firestore_controller.dart';
 import 'package:capstone/model/constants.dart';
 import 'package:capstone/model/kirby_user_model.dart';
-import 'package:capstone/model/history_screen_model.dart';
-import 'package:capstone/viewpage/health_info_screen.dart';
+import 'package:chart_components/chart_components.dart';
 import 'package:flutter/material.dart';
 
 import '../controller/auth_controller.dart';
+import '../model/history_screen_model.dart';
 
 class HistoryScreen extends StatefulWidget {
   static const routeName = "/history";
@@ -55,8 +55,34 @@ class _HistoryState extends State<HistoryScreen> {
   Widget historyScreenBody() {
     return Padding(
       padding: const EdgeInsets.all(8.0),
-      child: ListView(
-        children: [],
+      child: Container(
+        margin: EdgeInsets.all(16),
+        padding: EdgeInsets.only(bottom: 200, left: 8, right: 8, top: 20),
+        // decoration: BoxDecoration(
+        //   shape: BoxShape.rectangle,
+        //   borderRadius: BorderRadius.all(Radius.circular(10)),
+        //   border: Border.all(color: Theme.of(context).primaryColor),
+        // ),
+        child: BarChart(
+          data: [0, 1, 2, 3, 4, 5, 4],
+          labels: ["Su", "M", "Tu", "W", "Th", "F", "Sa"],
+          displayValue: true,
+          // labelStyle: TextStyle(fontSize: 18),
+          // valueStyle: TextStyle(fontSize: 12, fontWeight: FontWeight.bold),
+          reverse: true,
+          getColor: con.getColor,
+          getIcon: con.getIcon,
+          barWidth: 38,
+          barSeparation: 12,
+          animationDuration: Duration(milliseconds: 1800),
+          animationCurve: Curves.easeInOutSine,
+          itemRadius: 30,
+          iconHeight: 24,
+          footerHeight: 24,
+          headerValueHeight: 16,
+          roundValuesOnText: false,
+          lineGridColor: Colors.lightBlue,
+        ),
       ),
     );
   }
@@ -78,5 +104,35 @@ class _Controller {
       state.render(() => state.screenModel.loadingErrorMessage = "$e");
     }
     state.screenModel.loading = false;
+  }
+
+  Color getColor(double value) {
+    if (value < 2) {
+      return Colors.amber.shade300;
+    } else if (value < 4) {
+      return Colors.amber.shade600;
+    } else
+      return Colors.amber.shade900;
+  }
+
+  Icon getIcon(double value) {
+    if (value < 1) {
+      return Icon(
+        Icons.star_border,
+        size: 24,
+        color: getColor(value),
+      );
+    } else if (value < 2) {
+      return Icon(
+        Icons.star_half,
+        size: 24,
+        color: getColor(value),
+      );
+    } else
+      return Icon(
+        Icons.star,
+        size: 24,
+        color: getColor(value),
+      );
   }
 }
