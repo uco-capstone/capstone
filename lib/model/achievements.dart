ca5412c1a2de4053c82110972c0b127b2335d9b6
johnnylongvo <johnnylongvo@gmail.com>
2023-04-12 23:41:44 -0500

    created achievement class list

--- lib/model/achievements.dart
+++ lib/model/achievements.dart
@@ -1,1 +1,15 @@
-class Achievements {}
+class Achievement {
+  String name;
+  String description;
+  int progress;
+  int target; //
+  bool unlocked;
+
+  Achievement({
+    required this.name,
+    required this.description,
+    required this.progress,
+    required this.target,
+    required this.unlocked,
+  });
+}
