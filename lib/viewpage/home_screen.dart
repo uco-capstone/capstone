e39f3c2dd6021b6b83a0e1b5e2c06ec0a5856526
Vivian Nguyen <vivian.nguyen918@gmail.com>
2023-04-17 10:48:24 -0500

    UC-104 removed coins and Kirby position hardcode

--- lib/viewpage/home_screen.dart
+++ lib/viewpage/home_screen.dart
@@ -108,8 +108,7 @@ class _HomeScreenState extends State<HomeScreen> {
       onWillPop: () => Future.value(false),
       child: Scaffold(
         appBar: AppBar(
-          title: const Text('~My Kirby~'),
-          titleSpacing: 45,
+          title: const Center(child: Text("~My Kirby~"),),
           flexibleSpace: Container(
             decoration: const BoxDecoration(
               gradient: LinearGradient(
@@ -118,31 +117,6 @@ class _HomeScreenState extends State<HomeScreen> {
             ),
           ),
           actions: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Container(
-                width: 75,
-                decoration: const BoxDecoration(
-                    color: Colors.white,
-                    shape: BoxShape.rectangle,
-                    borderRadius: BorderRadius.all(Radius.circular(25))),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: const [
-                    //Sample number of coins
-                    Text(
-                      '0',
-                      style: TextStyle(
-                          color: Colors.black, fontWeight: FontWeight.bold),
-                    ),
-                    Icon(
-                      Icons.monetization_on,
-                      color: Colors.orangeAccent,
-                    ),
-                  ],
-                ),
-              ),
-            ),
             IconButton(
               onPressed: con.toDoListScreen,
               icon: const Icon(Icons.checklist_rounded),
@@ -205,6 +179,7 @@ class _HomeScreenState extends State<HomeScreen> {
 
   Widget body() {
     return Stack(
+      alignment: Alignment.center,
       children: [
         Container(
           //Background, if there is no configured background, then it'll show a default image
@@ -222,8 +197,7 @@ class _HomeScreenState extends State<HomeScreen> {
           ),
         ),
         Positioned(
-          top: 210,
-          left: 55,
+          bottom: MediaQuery.of(context).size.height * 0.15,
           //Pet, if there is no configured Kirby Pet, then it'll show a default image
           child: SizedBox(
               height: 300,
@@ -237,7 +211,6 @@ class _HomeScreenState extends State<HomeScreen> {
         Positioned(
           //Hunger gauge border
           top: 30,
-          left: 50,
           child: Container(
             color: Colors.white,
             height: 40,
@@ -247,7 +220,6 @@ class _HomeScreenState extends State<HomeScreen> {
         Positioned(
           //Hunger gauge body
           top: 32,
-          left: 52,
           child: Container(
             //Change color and size based off of percentage
             color: screenModel.kirbyPet!.hungerGauge > 7
@@ -261,7 +233,6 @@ class _HomeScreenState extends State<HomeScreen> {
         ),
         Positioned(
             top: 30,
-            left: 50,
             child: SizedBox(
               height: 40,
               width: 300,
